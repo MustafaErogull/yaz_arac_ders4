8caed31e41657c86843dcf1f5bfc311a37a03d55
Yunus Emre Doğan <yunusdogan.1907@hotmail.com>
2018-11-10 14:06:36 +0300

    spring boot proje ders 4

---
+++ ders4_proje/src/main/java/tr/edu/medipol/yazarac/ders4_proje/Ders4ProjeApplication.java
@@ -0,0 +1,12 @@
+package tr.edu.medipol.yazarac.ders4_proje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders4ProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders4ProjeApplication.class, args);
+	}
+}
